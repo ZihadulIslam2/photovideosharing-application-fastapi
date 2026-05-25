51365611ebebfb11daccd51515d97b14ddb36da2
Zihadul Islam <zihadul708@gmail.com>
2026-05-26 01:04:42 +0600

    Add file upload and feed retrieval endpoints with database integration

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse 
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,3 +12,45 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession= Depends(get_async_session)
+):
+    post = Post(
+        caption= caption,
+        url="dummy url",
+        file_type="photo",
+        file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession= Depends(get_async_session)
+):
+    result= await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_name": post.file_name,
+                "create_at": post.created_at.isoformat()
+            }
+        )
+
+        return { "posts": posts_data}
+    
+
